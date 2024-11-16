76a23bc26c40bec85d4d29df60c053ce7f51cfd9
ZerefRD <juanman.1305@gmail.com>
2024-11-16 16:55:00 -0400

    task 4

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -20,29 +20,33 @@ import static org.launchcode.techjobsmvc.controllers.ListController.columnChoice
 @Controller
 @RequestMapping("search")
 public class SearchController {
-
     @GetMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", columnChoices);
         return "search";
-
-}
+    }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
     @PostMapping("results")
-public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
-    ArrayList<Job> jobs;
-    if (searchType.equals("all") || searchTerm.isEmpty()){
-        jobs = JobData.findAll();
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        ArrayList<Job> jobs;
 
-    } else {
-        jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("searchType", searchType);
+        model.addAttribute("searchTerm", searchTerm);
+        model.addAttribute("title", "jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
+
+        if (!searchTerm.isEmpty()) {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+        // If search field is blank, there is nothing to filter jobs by, therefore no matter the column choice, all jobs will display
+        else {
+            jobs = JobData.findAll();
+        }
+        model.addAttribute("jobs", jobs);
+
+        return "search";
     }
-    model.addAttribute("jobs", jobs);
-    model.addAttribute("searchType", searchType);
-    model.addAttribute("searchTerm", searchTerm);
-    model.addAttribute("title", "jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
-    return "search";
-}
-}
+
+    }
